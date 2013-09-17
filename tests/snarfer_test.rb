4dbe1d7057f6137462f6294de32b4ee6788f3b1d
Oisin Hurley <oisin.hurley@gmail.com>
2013-09-17 22:48:32 +0100

    Fixed the log formats, which were incorrect, but this was masked by the fact the Amazon request id was not used.
    New test to check dups are not recorded.

--- tests/snarfer_test.rb
+++ tests/snarfer_test.rb
@@ -20,10 +20,10 @@ class SnarferTests < Minitest::Unit::TestCase
     LogDatabase.init("sqlite::memory:")
     @logs = []
     @logs << <<-eolog
-8021ec09afa691bca04f6a84f42ef094f8c2aa698d740694b71a7f8f6e149877 verbose-ireland  37.228.196.48 - 6472D7A57DB1174F REST.GET.OBJECT 01_TheVerbosePodcast_-_Episode01.mp3 "GET /verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3 HTTP/1.1" 206 - 50984143 50984143 18523 54 "https://s3-eu-west-1.amazonaws.com/verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31" -}
+8021ec09afa691bca04f6a84f42ef094f8c2aa698d740694b71a7f8f6e149877 verbose-ireland [23/May/2013:09:43:50 +0000] 37.228.196.48 - %s REST.GET.OBJECT 01_TheVerbosePodcast_-_Episode01.mp3 "GET /verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3 HTTP/1.1" 206 - 50984143 50984143 18523 54 "https://s3-eu-west-1.amazonaws.com/verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31" -}
 eolog
     @logs << <<-eolog
-8021ec09afa691bca04f6a84f42ef094f8c2aa698d740694b71a7f8f6e149877 verbose-ireland  37.228.196.48 - 6472D7A57DB11750 REST.GET.OBJECT 01_TheVerbosePodcast_-_Episode01.mp3 "GET /verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3 HTTP/1.1" 206 - 50984143 50984143 18523 54 "https://s3-eu-west-1.amazonaws.com/verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31" -}
+8021ec09afa691bca04f6a84f42ef094f8c2aa698d740694b71a7f8f6e149877 verbose-ireland [23/May/2013:09:43:50 +0000] 37.228.196.48 - %s REST.GET.OBJECT 01_TheVerbosePodcast_-_Episode01.mp3 "GET /verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3 HTTP/1.1" 206 - 50984143 50984143 18523 54 "https://s3-eu-west-1.amazonaws.com/verbose-ireland/01_TheVerbosePodcast_-_Episode01.mp3" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31" -}
 eolog
   end
 
@@ -49,17 +49,30 @@ eolog
   end
 
   def test_records_activity
-    now = Time.now.utc
-    yesterday = now.to_date - 1
-    
     # A couple of fake logs, activity should
     # indicate length of this array of logs
     # processed
     content = [
-      StringIO.new(@logs[0]),
-      StringIO.new(@logs[1])
+      StringIO.new(@logs[0] % "12345"),
+      StringIO.new(@logs[1] % "67890")
     ]
+    assert_recording(content, 2)
+  end
 
+  def test_does_not_record_twice
+    # Repeat the same log, checking that
+    # we don't record the same thing twice
+    content = [
+      StringIO.new(@logs[0] % "11111"),
+      StringIO.new(@logs[0] % "11111")
+    ]
+    assert_recording(content, 1)
+  end
+
+  def assert_recording(content, valid_count)
+    now = Time.now.utc
+    yesterday = now.to_date - 1
+    
     logs = MiniTest::Mock.new
     logs.expect(:with_prefix, content, ["logs/#{yesterday.strftime('%Y-%m-%d')}"])
 
@@ -78,7 +91,7 @@ eolog
         assert_equal 1, Activity.count
 
         activity = Activity.first
-        assert_equal 2, activity.processed
+        assert_equal valid_count, activity.processed
         assert_nil activity.exception
 
         # Comparing the following as strings to avoid
@@ -87,5 +100,7 @@ eolog
         assert_equal DateTime.now.to_s, activity.end.to_s
       end
     end
-  end  
+  end 
+
+
 end
